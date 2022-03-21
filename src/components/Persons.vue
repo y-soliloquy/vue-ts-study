a76d49d4609364363200ca24c973c8607487a618
y-soliloquy <engineer.soliloquy@gmail.com>
2022-03-21 15:00:54 +0900

    コンポーネントの用意

---
+++ src/components/Persons.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import PersonPostForm from './PersonPostForm.vue';
+import PersonList from './PersonList.vue';
+</script>
+
+<template>
+<div class="container">
+    <h1>Title</h1>
+    <PersonPostForm />
+    <div class="list-container">
+        <ul>
+            <PersonList />
+        </ul>
+    </div>
+</div>
+</template>
+
+<style scoped></style>
