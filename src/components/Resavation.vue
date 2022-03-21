971d4a3e23815c0b4672bdd425077ec7aadbe030
y-soliloquy <engineer.soliloquy@gmail.com>
2022-03-21 17:46:30 +0900

    コンポーネント化をしてUIを整理

--- src/components/Resavation.vue
+++ src/components/Resavation.vue
@@ -3,6 +3,8 @@ import { ref } from 'vue';
 import UITitle from './ui/UITitle.vue';
 import Card from './Card.vue';
 import Badge from './Badge.vue';
+import MosaicResavation from './MosaicResavation.vue';
+import ResavationList from './ResavationList.vue';
 
 const isListVisible = ref<boolean>(true);
 
@@ -25,25 +27,11 @@ const onClickButton = () => {
                 </Badge>
             </div>
         </Card>
-        <div v-if="isListVisible">
-            <Card class="resavation-card">
-                <span>John Wick1</span>
-                <Badge class="vip-badge">
-                    <span>VIP</span>
-                </Badge>
-            </Card>
-            <Card class="resavation-card">
-                <span>John Wick2</span>
-                <Badge class="normal-badge">
-                    <span>NORMAL</span>
-                </Badge>
-            </Card>
-            <Card class="resavation-card">
-                <span>John Wick3</span>
-                <Badge class="normal-badge">
-                    <span>NORMAL</span>
-                </Badge>
-            </Card>
+        <div v-if="!isListVisible">
+           <MosaicResavation />
+        </div>
+        <div v-else>
+            <ResavationList />
         </div>
 
 
@@ -68,17 +56,6 @@ const onClickButton = () => {
     margin-bottom: 24px;
 }
 
-.resavation-card {
-    width: 400px;
-    height: 60px;
-    background-color: rgb(255, 212, 147);
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 8px;
-    border-radius: 8px;
-    box-shadow: 4px 4px 4px rgb(211, 211, 211)
-}
 
 .badges {
     display: flex;
@@ -86,13 +63,5 @@ const onClickButton = () => {
     width: 60%;
     padding: 20px;
 }
-.vip-badge {
-    background-color: #810036;
-    color: white;
-}
-.normal-badge {
-    background-color: #002c8a;
-    color: white;
 
-}
 </style>
---
+++ src/components/ResavationList.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import Card from './Card.vue';
+import Badge from './Badge.vue';
+
+</script>
+
+<template>
+            <Card class="resavation-card">
+                <span>John Wick1</span>
+                <Badge class="vip-badge">
+                    <span>VIP</span>
+                </Badge>
+            </Card>
+            <Card class="resavation-card">
+                <span>John Wick2</span>
+                <Badge class="normal-badge">
+                    <span>NORMAL</span>
+                </Badge>
+            </Card>
+            <Card class="resavation-card">
+                <span>John Wick3</span>
+                <Badge class="normal-badge">
+                    <span>NORMAL</span>
+                </Badge>
+            </Card>
+
+</template>
+
+<style scoped>
+.resavation-card {
+    width: 400px;
+    height: 60px;
+    background-color: rgb(255, 212, 147);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 8px;
+    border-radius: 8px;
+    box-shadow: 4px 4px 4px rgb(211, 211, 211)
+}
+
+.vip-badge {
+    background-color: #810036;
+    color: white;
+}
+.normal-badge {
+    background-color: #002c8a;
+    color: white;
+
+}
+</style>
